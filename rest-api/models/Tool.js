612ef4d6da30c58553a83eb8a152a79ba7361c06
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-02-14 16:48:53 +0200

    Adjusting Tool Schema for 3d model URL

--- rest-api/models/Tool.js
+++ rest-api/models/Tool.js
@@ -6,6 +6,7 @@ const toolSchema = new Schema({
   country: { type: String, required: true },
   price: { type: Number, required: true },
   imgUrl: { type: String, require: true },
+  modelUrl: { type: String, required: true },
   description: { type: String, required: true },
   type: { type: String, required: true },
 });
