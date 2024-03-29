13f94c3157efc7dd6e169ac7c3dcb772aaa3ee6d
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-03-29 13:12:45 +0200

    Update environment.prod.ts

--- client-side/tools-catalogue/src/environments/environment.prod.ts
+++ client-side/tools-catalogue/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: 'https://tools-angular-project.vercel.app',
+  apiUrl:
+    'https://tools-angular-project-1s37yeotx-shadjipasevs-projects.vercel.app',
   production: true,
 };
