017e8c14f06d75d1ae9b31658a925f93427db9bb
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-03-31 12:13:39 +0300

    Update index.js

--- rest-api/index.js
+++ rest-api/index.js
@@ -34,7 +34,7 @@ async function start() {
   // app.use(cors())
 
   const corsOptions = {
-    origin: "*",
+    origin: "https://tools-co.web.app",
     methods: ["HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE"],
     Headers: [
       "Content-Type",
