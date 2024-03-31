e18fd434fc72d3d4142dcc26b94b7c94da716883
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-03-31 17:35:20 +0300

    Update index.js

--- rest-api/index.js
+++ rest-api/index.js
@@ -28,7 +28,7 @@ async function start() {
   const app = express();
   app.use(express.json());
   app.use(bodyParser.json());
-  app.use(express.json({ limit: "100mb" }));
+  // app.use(express.json({ limit: "100mb" }));
   app.use(express.urlencoded({ extended: false }));
 
   // app.use(cors())
@@ -62,5 +62,4 @@ async function start() {
   app.listen(5000, () => {
     console.log("REST service started");
   });
-  // module.export =
 }
