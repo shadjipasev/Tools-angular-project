35cb435e17e69d409a0a4cc2ff01b121de24d811
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-04-02 23:46:52 +0300

    Update toolController.js

--- rest-api/controllers.js/toolController.js
+++ rest-api/controllers.js/toolController.js
@@ -147,7 +147,6 @@ toolController.get("/search/:query", async (req, res) => {
 
 toolController.get("/download/:fileId", async (req, res) => {
   const fileId = req.params.fileId;
-  let fileName = "shitfile";
 
   try {
     let dbRef = mongoose.connections[0].db;
@@ -161,12 +160,10 @@ toolController.get("/download/:fileId", async (req, res) => {
     downloadStream.pipe(res);
 
     downloadStream.on("modelFile", (file) => {
-      res.set({
-        "Content-Disposition": `attachment; filename='${fileName}'`,
-        "Content-Type": file.contentType,
-      });
+      downloadStream.pipe(res);
+      res.set("Content-Type", file.contentType);
+      res.set({ "Content-Disposition": `attachment; filename='${fileName}'` });
     });
-    downloadStream.pipe(res);
 
     // bucketRef.openDownloadStream(new mongoose.Types.ObjectId(fileId)).pipe(res);
   } catch (error) {
