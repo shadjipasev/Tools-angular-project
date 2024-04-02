86fa5f7699e325f97e48ae2ccde4a94ffe477e69
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-04-03 00:59:53 +0300

    Update details.component.ts

--- client-side/tools-catalogue/src/app/main/details/details.component.ts
+++ client-side/tools-catalogue/src/app/main/details/details.component.ts
@@ -88,7 +88,7 @@ export class DetailsComponent implements OnInit {
   downloadFile(fileId: string, toolName: string, event: MouseEvent) {
     event.preventDefault();
     console.log(fileId);
-    let fileName = toolName.toLowerCase();
+    // let fileName = toolName.toLowerCase();
     // console.log(this.tool.name);
 
     // ----------------------------
@@ -99,7 +99,7 @@ export class DetailsComponent implements OnInit {
     //-----------------------------
     this.toolService.downloadFile(fileId).subscribe((response: any) => {
       // Use FileSaver.js to save the blob as a file
-      saveAs(response, fileName);
+      saveAs(response, 'shit');
     });
     // (error) => {
     //   const options = 'Failed to download file. Please try again later.';
