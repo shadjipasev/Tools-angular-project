61f0a8cdc25c32487c3b429e8a257688eb1d54a3
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-04-05 01:20:13 +0300

    .

--- client-side/tools-catalogue/src/app/app.module.ts
+++ client-side/tools-catalogue/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { SharedModule } from './shared/shared.module';
-import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 // import { MatInputModule } from '@angular/material/input';
 // import { MatButtonModule } from '@angular/material/button';
