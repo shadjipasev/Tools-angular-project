bb082d14ed767e7088fd3d9d51d742818cee5f17
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-04-04 23:56:53 +0300

    Update shared.module.ts

--- client-side/tools-catalogue/src/app/shared/shared.module.ts
+++ client-side/tools-catalogue/src/app/shared/shared.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
-import { AppModule } from '../app.module';
 import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
