95eec77919753da99205fe3cdabc8e26cbe1180b
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-07-14 15:30:38 +0300

    Update catalog-personal-equipment.component.ts

--- client-side/tools-catalogue/src/app/main/catalog-personal-equipment/catalog-personal-equipment.component.ts
+++ client-side/tools-catalogue/src/app/main/catalog-personal-equipment/catalog-personal-equipment.component.ts
@@ -4,19 +4,18 @@ import { ToolService } from '../services/tool/tool.service';
 @Component({
   selector: 'app-catalog-personal-equipment',
   templateUrl: './catalog-personal-equipment.component.html',
-  styleUrls: ['./catalog-personal-equipment.component.css']
+  styleUrls: ['./catalog-personal-equipment.component.css'],
 })
 export class CatalogPersonalEquipmentComponent implements OnInit {
-
   tools: any;
 
-  constructor(private toolService: ToolService) { }
+  constructor(private toolService: ToolService) {}
 
   ngOnInit(): void {
-    this.toolService.getToolsByType('Personal and Protective equipment').subscribe(res => {
-      this.tools = res,
-      (error: any) => console.log(error);
-    })
+    this.toolService
+      .getToolsByType('Personal/Protective equipment')
+      .subscribe((res) => {
+        (this.tools = res), (error: any) => console.log(error);
+      });
   }
-
 }
