9114b6e0dc008753f4b6d0f27b43d4c5c608ea40
Spasidar Hadzhipase <shadjipasev@gmail.com>
2024-07-14 20:08:05 +0300

    Fixing redirection after delete

--- client-side/tools-catalogue/src/app/main/delete-tool/delete-tool.component.ts
+++ client-side/tools-catalogue/src/app/main/delete-tool/delete-tool.component.ts
@@ -5,23 +5,24 @@ import { ToolService } from '../services/tool/tool.service';
 @Component({
   selector: 'app-delete-tool',
   templateUrl: './delete-tool.component.html',
-  styleUrls: ['./delete-tool.component.css']
+  styleUrls: ['./delete-tool.component.css'],
 })
 export class DeleteToolComponent implements OnInit {
-
   toolId: any;
 
-  constructor(private route: ActivatedRoute, private toolService: ToolService, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private toolService: ToolService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.toolId = this.route.snapshot.params['id'];
 
-    this.toolService.deleteTool(this.toolId).subscribe(res => {
+    this.toolService.deleteTool(this.toolId).subscribe((res) => {
       console.log(`${res} ---- DELETED`),
-      (error: any) => console.log(error),
-      this.router.navigateByUrl('data/catalog')
-    })
-    
+        (error: any) => console.log(error),
+        this.router.navigateByUrl('/');
+    });
   }
-
 }
